530cf710161b92bb7688d9e40ac944e12d062749
KH4LI <KH4LI@DESKTOP-FVIMQLE>
2021-06-04 17:15:28 +0430

    Code final edits

--- Image2Cordinates/Image2Cordinates.py
+++ Image2Cordinates/Image2Cordinates.py
@@ -6,16 +6,30 @@ Created on Mon May 10 07:04:20 2021
 """
 
 import cv2
+import os
 
-img = cv2.imread('map.png')
+mapfile = input("Map file? ")
+img = cv2.imread(mapfile);
+if img is None:
+    print ("Error opening image!")
+    input("Press Enter to exit...")
+    exit(-1);
 xys = []
+t_xys = []
 w = len(img)
 h = len(img[0,:])
 for i in range(w):
     for j in range(h):
-        if img[i,j,0] == 0:
+        if sum(img[i,j,:]) == 0:
             xys.append(str(i) + "\n")
             xys.append(str(j) + "\n")
-f = open('map.txt','w')
+        elif img[i,j,2] - (img[i,j,1] + img[i,j,0]) > 50:
+            t_xys.append(str(i) + "\n")
+            t_xys.append(str(j) + "\n")
+
+f = open(os.path.splitext(mapfile)[0] + '.cxy','w')
 f.writelines(xys)
+f.write("---TARGETS---\n");
+f.writelines(t_xys)
 f.close()
+print ("Done!")
